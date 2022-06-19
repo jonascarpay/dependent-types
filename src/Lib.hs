3131c8cf30e2ee2bde1e412fb129cae4bf766554
Jonas Carpay <jonascarpay@gmail.com>
2022-06-20 04:02:01 +0900

    Type checking

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,14 +2,17 @@
 
 module Lib where
 
+import Control.Monad
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.String (IsString (..))
 
 data Type
-  = TUnit
+  = TBase String
   | TFun Type Type
   deriving (Eq, Show)
+
+instance IsString Type where fromString = TBase
 
 data Expr
   = Typed Expr Type
@@ -18,8 +21,7 @@ data Expr
   | Lam String Expr
   deriving (Eq, Show)
 
-instance IsString Expr where
-  fromString = Var
+instance IsString Expr where fromString = Var
 
 data Value
   = Neutral Neutral
@@ -45,11 +47,6 @@ eval (Typed e _) = eval e
 eval (Var str) = Neutral (NVar str)
 eval (Lam str body) = VLam str (eval body)
 
-substN :: String -> Value -> Neutral -> Neutral
-substN var sub = go
-  where
-    go = undefined
-
 subst :: String -> Value -> Value -> Value
 subst var sub = goV
   where
@@ -69,3 +66,46 @@ subst var sub = goV
 
 -- >>> eval $ (Lam "const" (Lam "id" (App "const" "id" `App` "y")) `App` Lam "a" (Lam "b" "a")) `App` Lam "y" "y"
 -- VLam "y" (Neutral (NVar "y"))
+
+data Context
+  = Empty
+  | TyCons String Context
+  | TrCons String Type Context
+  deriving (Show)
+
+valid :: Context -> Bool
+valid Empty = True
+valid (TyCons _ ctx) = valid ctx
+valid (TrCons x τ ctx) = valid ctx && wellFormed ctx τ
+
+wellFormed :: Context -> Type -> Bool
+wellFormed ctx (TBase str) = isBaseIn ctx
+  where
+    isBaseIn Empty = False
+    isBaseIn (TyCons str' ctx') = str == str' || isBaseIn ctx'
+    isBaseIn (TrCons str' _ ctx') = str /= str' && isBaseIn ctx'
+wellFormed ctx (TFun τ τ') = wellFormed ctx τ && wellFormed ctx τ'
+
+hasType :: Context -> Expr -> Type -> Bool
+hasType ctx (Lam str body) (TFun tf tx) = hasType (TrCons str tf ctx) body tx
+hasType ctx expr ty = infer ctx expr == Just ty
+
+infer :: Context -> Expr -> Maybe Type
+infer ctx (Typed expr ty)
+  | wellFormed ctx ty && hasType ctx expr ty = Just ty
+  | otherwise = Nothing
+infer ctx (Var str) = go ctx
+  where
+    go Empty = Nothing
+    go (TyCons str' ctx')
+      | str == str' = Nothing
+      | otherwise = go ctx'
+    go (TrCons str' ty ctx')
+      | str == str' = pure ty
+      | otherwise = go ctx'
+infer ctx (App f x) = case infer ctx f of
+  Just (TFun τ τ') | hasType ctx x τ -> Just τ'
+  _ -> Nothing
+infer ctx (Lam _ _) = Nothing -- Unannotated lambda expression
+
+-- >>> infer (TrCons "y" "a" $ TyCons "a" Empty) ()
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
+import Data.Coerce
 import qualified Data.Set as Set
 import Lib
 import Test.Hspec
@@ -11,28 +13,50 @@ import Test.QuickCheck
 -- Test:
 -- forall e1 e1 x, x not in (free e1), (\x -> e1) e2 = e1
 
-newtype VarStr = VarStr {getVarStr :: String}
+newtype VarStr = VarStr {_getVarStr :: String}
   deriving newtype (Show)
 
-instance Arbitrary VarStr where
-  arbitrary = elements [VarStr (show x) | x <- [1 :: Int .. 7]]
+instance Arbitrary VarStr where arbitrary = coerce var
+
+var :: Gen String
+var = elements [show x | x <- [1 :: Int .. 7]]
 
 instance Arbitrary Expr where
   arbitrary = sized f
     where
-      var = getVarStr <$> arbitrary
       f 0 = Var <$> arbitrary
       f n =
-        let n' = div n 3 * 2
-         in oneof
-              [ App <$> f n' <*> f n',
-                Lam <$> var <*> f n',
-                Var <$> var
-              ]
+        let r = f (div n 3 * 2)
+         in oneof [App <$> r <*> r, Lam <$> var <*> r, Var <$> var]
   shrink (Var str) = Var <$> shrink str
   shrink (App f x) = [f, x]
   shrink (Lam str body) = body : (Lam str <$> shrink body)
   shrink (Typed e _) = [e]
+
+instance Arbitrary Context where
+  arbitrary = sized (\n -> chooseInt (0, n)) >>= f
+    where
+      f 0 = pure Empty
+      f n =
+        oneof
+          [ TyCons <$> var <*> f (n - 1),
+            TrCons <$> var <*> arbitrary <*> f (n - 1)
+          ]
+  shrink = fmap fromList . shrink . toList
+    where
+      toList Empty = []
+      toList (TyCons str ctx) = Left str : toList ctx
+      toList (TrCons str ty ctx) = Right (str, ty) : toList ctx
+
+      fromList = foldr (either TyCons (uncurry TrCons)) Empty
+
+instance Arbitrary Type where
+  arbitrary = sized f
+    where
+      f 0 = TBase <$> var
+      f n =
+        let r = f (div n 2)
+         in oneof [TBase <$> var, TFun <$> r <*> r]
 
 main :: IO ()
 main =
@@ -41,3 +65,22 @@ main =
       prop "abstract and apply" $ \e1 e2 (VarStr str) -> Set.notMember str (free e1) ==> eval e1 == eval (App (Lam str e1) e2)
       prop "id" $ \e (VarStr str) -> eval e == eval (Lam str (Var str) `App` e)
       prop "const" $ \ea eb (VarStr a) (VarStr b) -> a /= b && Set.notMember b (free ea) ==> eval ea == eval ((Lam a (Lam b (Var a)) `App` ea) `App` eb)
+      describe "id type example" $ do
+        let ctx = TrCons "y" "α" $ TyCons "α" Empty
+            idExpr = Lam "x" "x"
+            idTyp = TFun "α" "α"
+            expr = App (Typed idExpr idTyp) "y"
+        it "hasType" (hasType ctx expr "α")
+        it "infers" (infer ctx expr == Just "α")
+      describe "const type example" $ do
+        let ctx = TyCons "β" $ TrCons "y" "α" $ TyCons "α" Empty
+            idExpr = Lam "x" "x"
+            constTyp = TFun (TFun "β" "β") (TFun "α" (TFun "β" "β"))
+            constExpr = Lam "a" (Lam "b" "a")
+            expr = App (App (Typed constExpr constTyp) idExpr) "y"
+        it "hasType" (hasType ctx expr (TFun "β" "β"))
+        it "infers" (infer ctx expr `shouldBe` Just (TFun "β" "β"))
+      prop "infer infers correct type" $ \ctx expr ->
+        case infer ctx expr of
+          Just ty -> hasType ctx expr ty
+          Nothing -> True
