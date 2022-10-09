a5ea2f0e1e075a6499e969547c394426c0d74ddf
Jonas Carpay <jonascarpay@gmail.com>
2022-10-10 01:25:57 +0900

    Refactor

--- src/Lib.hs
+++ src/Lib.hs
@@ -33,6 +33,12 @@ data Neutral
   | NApp Neutral Value
   deriving (Eq, Show)
 
+data Context
+  = Empty
+  | HasKind String Context
+  | HasType String Type Context
+  deriving (Show)
+
 free :: Expr -> Set String
 free (Var str) = Set.singleton str
 free (App f x) = free f <> free x
@@ -67,45 +73,39 @@ subst var sub = goV
 -- >>> eval $ (Lam "const" (Lam "id" (App "const" "id" `App` "y")) `App` Lam "a" (Lam "b" "a")) `App` Lam "y" "y"
 -- VLam "y" (Neutral (NVar "y"))
 
-data Context
-  = Empty
-  | TyCons String Context
-  | TrCons String Type Context
-  deriving (Show)
-
 valid :: Context -> Bool
 valid Empty = True
-valid (TyCons _ ctx) = valid ctx
-valid (TrCons x τ ctx) = valid ctx && wellFormed ctx τ
+valid (HasKind _ ctx) = valid ctx
+valid (HasType x τ ctx) = valid ctx && isType ctx τ
 
-wellFormed :: Context -> Type -> Bool
-wellFormed ctx (TBase str) = isBaseIn ctx
+isType :: Context -> Type -> Bool
+isType ctx (TBase str) = isBaseIn ctx
   where
     isBaseIn Empty = False
-    isBaseIn (TyCons str' ctx') = str == str' || isBaseIn ctx'
-    isBaseIn (TrCons str' _ ctx') = str /= str' && isBaseIn ctx'
-wellFormed ctx (TFun τ τ') = wellFormed ctx τ && wellFormed ctx τ'
+    isBaseIn (HasKind str' ctx') = str == str' || isBaseIn ctx'
+    isBaseIn (HasType str' _ ctx') = str /= str' && isBaseIn ctx'
+isType ctx (TFun τ τ') = isType ctx τ && isType ctx τ'
 
-hasType :: Context -> Expr -> Type -> Bool
-hasType ctx (Lam str body) (TFun tf tx) = hasType (TrCons str tf ctx) body tx
-hasType ctx expr ty = infer ctx expr == Just ty
+typeCheck :: Context -> Expr -> Type -> Bool
+typeCheck ctx (Lam str body) (TFun tf tx) = typeCheck (HasType str tf ctx) body tx
+typeCheck ctx expr ty = infer ctx expr == Just ty
 
 infer :: Context -> Expr -> Maybe Type
 infer ctx (Typed expr ty)
-  | wellFormed ctx ty && hasType ctx expr ty = Just ty
+  | isType ctx ty && typeCheck ctx expr ty = Just ty
   | otherwise = Nothing
 infer ctx (Var str) = go ctx
   where
     go Empty = Nothing
-    go (TyCons str' ctx')
+    go (HasKind str' ctx')
       | str == str' = Nothing
       | otherwise = go ctx'
-    go (TrCons str' ty ctx')
+    go (HasType str' ty ctx')
       | str == str' = pure ty
       | otherwise = go ctx'
 infer ctx (App f x) = case infer ctx f of
-  Just (TFun τ τ') | hasType ctx x τ -> Just τ'
+  Just (TFun τ τ') | typeCheck ctx x τ -> Just τ'
   _ -> Nothing
 infer ctx (Lam _ _) = Nothing -- Unannotated lambda expression
 
--- >>> infer (TrCons "y" "a" $ TyCons "a" Empty) ()
+-- >>> infer (HasType "y" "a" $ HasKind "a" Empty) ()
